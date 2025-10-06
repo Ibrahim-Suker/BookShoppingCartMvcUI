8ff82f254e6a0ed08046eb24a4b24bc6bc9f15e8
Ibrahim-Suker <ibrahimsukeroo@gmail.com>
2025-10-06 19:06:49 +0300

    Add project files.

---
+++ BookShoppingCartMvcUI/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using BookShoppingCartMvcUI.Data;
+global using BookShoppingCartMvcUI.Models;
+global using Microsoft.AspNetCore.Identity;
+global using BookShoppingCartMvcUI.Repositories;
+global using Microsoft.EntityFrameworkCore;
+global using BookShoppingCartMvcUI.Models.DTOs;
+global using BookShoppingCartMvcUI.Constants;
---
+++ BookShoppingCartMvcUI/Program.cs
@@ -0,0 +1,66 @@
+﻿using BookShoppingCartMvcUI;
+using BookShoppingCartMvcUI.Data;
+using BookShoppingCartMvcUI.Repositories;
+using BookShoppingCartMvcUI.Shared;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services
+    .AddIdentity<IdentityUser,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders();
+builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IHomeRepository, HomeRepository>();
+builder.Services.AddTransient<ICartRepository, CartRepository>();
+builder.Services.AddTransient<IUserOrderRepository, UserOrderRepository>();
+builder.Services.AddTransient<IStockRepository, StockRepository>();
+builder.Services.AddTransient<IGenreRepository, GenreRepository>();
+builder.Services.AddTransient<IFileService, FileService>();
+builder.Services.AddTransient<IBookRepository, BookRepository>();
+builder.Services.AddTransient<IReportRepository, ReportRepository>();
+
+
+
+var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    await DbSeeder.SeedDefaultData(services);
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthentication(); // ✅ مهم قبل Authorization
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
